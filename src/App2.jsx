e30834f6a949fed323481cbb21241edfe07717ef
thaissthephanye <thaissthephanye@gmail.com>
2025-02-28 17:29:20 -0300

    fist commit

---
+++ src/App2.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+
+//ctrl + ; = comentário no vscode
+
+//rafce
+
+//Arrow function
+//const app2 = () => <h1>olá, mundo!</h1>;
+
+//nomeação de variável, função...
+//camelCase
+//primeira minúscula e as demais maiúsculas
+
+//nomeação de componente
+//PascalCase
+//todas as palavras juntas e a primeira em maiúscula
+
+//export default, posso importar com qualquer nome e sem chaves
+//export sem default, só posso importar entre chaves e com o nome exportado
+
+// selfie closing tag
+//<Header></Header>
+//<Header />
+
+//colocar javascript no meio do html no jsx, utiliza-se chaves {}
+
+//tem um root é react
+
+//nomeação de classes em CSS
+//metodologia BEM
+//blocks
+//elements
+//modifier
+//bloco__elemento--modificador
+//header__link
+//header__link--small
+// nomes compostos são separados por -
+
+//tag vazia em React se chama Fragment ou Fragmento
